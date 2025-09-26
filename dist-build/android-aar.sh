293f5da64a2a763e8133542764cd64d850e44511
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-09-26 16:05:33 +0300

    Added RiV-chain/artifact publishing job for the libsodium aar library

--- dist-build/android-aar.sh
+++ dist-build/android-aar.sh
@@ -1,194 +1,89 @@
 #! /bin/sh
 
-# Create an AAR with libsodium in all combinations of static | shared | minimal | full.
-#
-# To simplify linking, library variants have distinct names: sodium, sodium-static, sodium-minimal and sodium-minimal-static.
+# Build a plain AAR containing only full libsodium.so variants for multiple ABIs.
+# The resulting AAR is created in the current working directory.
 
 SODIUM_VERSION="1.0.21.0"
-
-if [ -z "$ANDROID_NDK_HOME" ]; then
-  echo "ANDROID_NDK_HOME must be set to the directory containing the Android NDK."
-  exit 1
-fi
-
-if [ ! -f "${ANDROID_NDK_HOME}/ndk-build" ]; then
-  echo "The ANDROID_NDK_HOME directory does not contain an 'ndk-build' file."
-  exit 1
-fi
-
-if [ ! -d "${ANDROID_NDK_HOME}/toolchains" ]; then
-  echo "The ANDROID_NDK_HOME directory does not contain a 'toolchains' directory."
-  exit 1
-fi
-
-if [ ! -f "${ANDROID_NDK_HOME}/source.properties" ]; then
-  echo "The ANDROID_NDK_HOME directory does not contain a 'source.properties' file."
-  exit 1
-fi
-
-NDK_VERSION=$(grep "Pkg.Revision = " <"${ANDROID_NDK_HOME}/source.properties" | cut -f 2 -d '=' | cut -f 2 -d' ' | cut -f 1 -d'.')
 DEST_PATH=$(mktemp -d)
+AAR_PATH="$(pwd)/libsodium-${SODIUM_VERSION}.aar"
 
+# Default Android NDK platform
 if [ -z "$NDK_PLATFORM" ]; then
   export NDK_PLATFORM="android-21"
-  echo "Compiling for default platform: [${NDK_PLATFORM}]"
-  echo "That can be changed by setting an NDK_PLATFORM environment variable."
+  echo "Compiling for default platform: [$NDK_PLATFORM]"
 fi
-
 SDK_VERSION=$(echo "$NDK_PLATFORM" | cut -f2 -d"-")
-if [ -z "$NDK_VERSION" ]; then
-  echo "Failed to determine the NDK version."
-  exit 1
-fi
-if [ -z "$SDK_VERSION" ]; then
-  echo "Failed to determine the SDK version."
-  exit 1
-fi
-echo "NDK version: [$NDK_VERSION]"
-echo "SDK version: [$SDK_VERSION]"
-
-echo
-
-if which zip >/dev/null; then
-  echo "The 'zip' command is installed."
-else
-  echo "The 'zip' command is not installed. Please install it before running this script."
-  exit 1
-fi
 
 cd "$(dirname "$0")/../" || exit
+trap 'rm -rf "$DEST_PATH"; exit' INT TERM EXIT
 
-trap 'kill -TERM -$$' INT
+# Minimal AndroidManifest.xml
+cat <<EOF >"$DEST_PATH/AndroidManifest.xml"
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.goterl.lazysodium"
+    android:versionCode="1"
+    android:versionName="1.0">
+    <uses-sdk android:minSdkVersion="$SDK_VERSION" android:targetSdkVersion="$SDK_VERSION"/>
+</manifest>
+EOF
 
-make_abi_json() {
-  echo "{\"abi\":\"${NDK_ARCH}\",\"api\":${SDK_VERSION},\"ndk\":${NDK_VERSION},\"stl\":\"none\"}" >"$1/abi.json"
-}
+# META-INF folder with LICENSE
+mkdir -p "$DEST_PATH/META-INF"
+cp LICENSE "$DEST_PATH/META-INF"
 
-make_prefab_json() {
-  echo "{\"name\":\"sodium\",\"schema_version\":1,\"dependencies\":[],\"version\":\"$SODIUM_VERSION\"}" >"$1/prefab.json"
-}
+# Create jni folders for ABIs
+for abi in "armeabi-v7a" "arm64-v8a" "x86" "x86_64"; do
+  mkdir -p "$DEST_PATH/jni/$abi"
+done
 
-make_manifest() {
-  echo "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"com.android.ndk.thirdparty.sodium\" android:versionCode=\"1\" android:versionName=\"1.0\">
-        <uses-sdk android:minSdkVersion=\"$SDK_VERSION\" android:targetSdkVersion=\"$SDK_VERSION\"/>
-</manifest>" >"${1}/AndroidManifest.xml"
-}
+# --- Run cross-compilation scripts to produce full libsodium.so ---
+echo "Building full libsodium binaries..."
+dist-build/android-armv7-a.sh
+dist-build/android-armv8-a.sh
+dist-build/android-x86.sh
+dist-build/android-x86_64.sh
 
-make_prefab_structure() {
-  for variant_dirs in "prefab" "prefab/modules" "META-INF"; do
-    mkdir "${DEST_PATH}/${variant_dirs}"
-  done
-
-  make_prefab_json "${DEST_PATH}/prefab"
-  make_manifest "${DEST_PATH}"
-  cp "LICENSE" "${DEST_PATH}/META-INF"
-
-  for variant in \
-    "prefab/modules/sodium" "prefab/modules/sodium-static" \
-    "prefab/modules/sodium-minimal" "prefab/modules/sodium-minimal-static"; do
-    mkdir "${DEST_PATH}/${variant}"
-
-    if [ "$variant" = "prefab/modules/sodium-minimal" ]; then
-      echo "{\"library_name\":\"libsodium\"}" >"${DEST_PATH}/${variant}/module.json"
-    else
-      echo "{}" >"${DEST_PATH}/${variant}/module.json"
-    fi
-
-    mkdir "${DEST_PATH}/${variant}/libs"
-
-    for arch in "arm64-v8a" "armeabi-v7a" "x86" "x86_64"; do
-      mkdir "$DEST_PATH/${variant}/libs/android.${arch}"
-      mkdir "$DEST_PATH/${variant}/libs/android.${arch}/include"
-      NDK_ARCH="$arch"
-
-      make_abi_json "$DEST_PATH/${variant}/libs/android.${arch}"
-    done
-  done
-}
-
+# --- Function to copy compiled libsodium .so into jni/<abi> ---
 copy_libs() {
-  SRC_DIR="libsodium-android-${1}"
+  build_hint="$1"   # e.g. armv7-a
+  abi="$2"          # e.g. armeabi-v7a
 
-  SHARED_DEST_DIR="${DEST_PATH}/prefab/modules/sodium${3}/libs/android.${2}"
-  STATIC_DEST_DIR="${DEST_PATH}/prefab/modules/sodium${3}-static/libs/android.${2}"
+  SRC_DIR="libsodium-android-${build_hint}"
+  src_so="${SRC_DIR}/lib/libsodium.so"
+  dest_so="${DEST_PATH}/jni/${abi}/libsodium.so"
 
-  cp -r "${SRC_DIR}/include" "$SHARED_DEST_DIR"
-  cp -r "${SRC_DIR}/include" "$STATIC_DEST_DIR"
-  cp "${SRC_DIR}/lib/libsodium.so" "${SHARED_DEST_DIR}/libsodium.so"
-  cp "${SRC_DIR}/lib/libsodium.a" "${STATIC_DEST_DIR}/libsodium${3}-static.a"
+  if [ ! -f "$src_so" ]; then
+    echo "Error: $src_so not found. Did dist-build/${build_hint}.sh run correctly?"
+    exit 1
+  fi
 
-  rm -r "$SRC_DIR"
+  echo "Copying: $src_so -> $dest_so"
+  cp "$src_so" "$dest_so"
 }
 
-build_all() {
-  dist-build/android-armv7-a.sh
-  dist-build/android-armv8-a.sh
-  dist-build/android-x86_64.sh
-  dist-build/android-x86.sh
-}
-
-make_prefab_structure
-
-build_all
-
-copy_libs "armv7-a" "armeabi-v7a" "-minimal"
-copy_libs "armv8-a+crypto" "arm64-v8a" "-minimal"
-copy_libs "i686" "x86" "-minimal"
-copy_libs "westmere" "x86_64" "-minimal"
-
-LIBSODIUM_FULL_BUILD="Y"
-export LIBSODIUM_FULL_BUILD
-
-build_all
-
+# Copy only full variants
 copy_libs "armv7-a" "armeabi-v7a"
 copy_libs "armv8-a+crypto" "arm64-v8a"
 copy_libs "i686" "x86"
 copy_libs "westmere" "x86_64"
 
-AAR_PATH="$(pwd)/libsodium-${SODIUM_VERSION}.aar"
+# Package into AAR directly in current working directory
 cd "$DEST_PATH" || exit
-rm "$AAR_PATH"
-zip -9 -r "$AAR_PATH" META-INF prefab AndroidManifest.xml
-cd .. || exit
-rm -r "$DEST_PATH"
+zip -9 -r "$AAR_PATH" AndroidManifest.xml META-INF jni
+cd ..
+
+# Cleanup temporary build folder
+rm -rf "$DEST_PATH"
 
 echo "
+Congrats! Plain AAR built at:
+$AAR_PATH
 
-Congrats you have built an AAR containing libsodium!
-The build used a min Android SDK of version $SDK_VERSION
-You can build for a different SDK version by specifying NDK_PLATFORM=\"android-{SDK_VERSION}\"
-as an environment variable before running this script but the defaults should be fine.
+Contains libsodium.so for ABIs: armeabi-v7a, arm64-v8a, x86, x86_64 (full variants only)
 
-To use the aar with
-gradle or cmake (as set by default for Android Studio projects):
+Usage in Gradle:
 
-- Edit the app/build.gradle file to add:
-
-    android {
-        buildFeatures {
-            prefab true
-        }
-    }
-
-  and
-
-    dependencies {
-        implementation fileTree(dir:'path/to/aar/',include:['libsodium-$SODIUM_VERSION.aar'])
-    }
-
-  Optionally, store multiple AAR files in the same folder and include '*.aar'
-
-- Edit your module's CMakeLists.txt file to add:
-
-    find_package(sodium REQUIRED CONFIG)
-
-- Then, specify 'sodium::x' as an item in the relevant 'target_link_libraries' statement.
-    The first part is the AAR name and should be 'sodium'.
-    The second part ('x', to be replaced) should be set to:
-      - 'sodium' for the full shared library,
-      - 'sodium-static' for the full static library
-      - 'sodium-minimal' for the minimal shared library, or
-      - 'sodium-minimal-static' for the minimal static library.
-
-  "
+dependencies {
+    implementation files('$AAR_PATH')
+}
+"
